0620b1aa50f05b3f59c0c16c9d44e2f6fb734fed
Henry Garcia <mocqponerxqsi@gmail.com>
2025-03-01 23:34:21 -0500

    blablabla

--- preview.js
+++ preview.js
@@ -3,11 +3,14 @@ function verification() {
 const Xada = "Xada";
 const Esteban = "Esteban";
 const Leidys = "Leidys";
+const Henry = "Henry";
     if (password === Xada || password === Leidys || password === Esteban) {
         const paragraph = document.getElementById("paragraph");
         paragraph.innerHTML =
             "¡Oops! : [ Parece que intentaste entrar antes de tiempo, la página aún no esta lista (pero estoy trabajando en ella🧑‍💻, espera un poco más😉).";
+    } else if (password === Henry) {
+        paragraph.innerHTML = "🤨 ¡Tú no eres yo!";
     } else {
-        paragraph.innerHTML = "🤨 ¡Escríbelo bien! ¡ES TU NOMBRE!";
+      paragraph.innerHTML = "🤨 ¡Escríbelo bien! ¡ES TU NOMBRE!";
     }
 }
